9a7cd0b6a21048d45d35d1b025e92c8bd9caa1e5
Todor Stoyanov <todor.stoyanov@oru.se>
2013-11-28 16:29:46 +0100

    Rebase from 'debian/groovy/ndt_fuser'

--- include/ndt_fuser/ndt_fuser.h
+++ include/ndt_fuser/ndt_fuser.h
@@ -1,11 +1,11 @@
 #ifndef NDT_FUSER_HH
 #define NDT_FUSER_HH
 
-#include <ndt_viz.h>
-#include <ndt_map.h>
-#include <ndt_matcher_d2d_2d.h>
-#include <ndt_matcher_d2d.h>
-#include <pointcloud_utils.h>
+#include <ndt_visualisation/ndt_viz.h>
+#include <ndt_map/ndt_map.h>
+#include <ndt_registration/ndt_matcher_d2d_2d.h>
+#include <ndt_registration/ndt_matcher_d2d.h>
+#include <pointcloud_vrml/pointcloud_utils.h>
 
 #include <Eigen/Eigen>
 #include <pcl/point_cloud.h>
