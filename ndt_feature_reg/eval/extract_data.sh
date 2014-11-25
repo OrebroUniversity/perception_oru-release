20da3f47801d76ad5c3489b491d24a526b9c05a5
Todor Stoyanov <todor.stoyanov@oru.se>
2014-11-25 22:39:19 +0100

    Trimmed the branch to only the ndt_map sub directory

--- ndt_feature_reg/eval/extract_data.sh
+++
@@ -1,8 +0,0 @@
-# Unpack
-tar xvfz rgbd_dataset_freiburg1_xyz.tgz
-tar xvfz rgbd_dataset_freiburg1_rpy.tgz
-tar xvfz rgbd_dataset_freiburg1_360.tgz
-tar xvfz rgbd_dataset_freiburg1_floor.tgz
-tar xvfz rgbd_dataset_freiburg1_desk.tgz
-tar xvfz rgbd_dataset_freiburg1_desk2.tgz
-tar xvfz rgbd_dataset_freiburg1_room.tgz
