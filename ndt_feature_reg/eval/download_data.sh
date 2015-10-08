3317af8a17331360bfc34f1afcc913a9caba998f
Todor Stoyanov <todor.stoyanov@oru.se>
2015-10-08 14:08:25 +0200

    Trimmed the branch to only the sdf_tracker sub directory

--- ndt_feature_reg/eval/download_data.sh
+++
@@ -1,8 +0,0 @@
-# Download the data
-wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_xyz.tgz
-wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_rpy.tgz
-wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_360.tgz
-wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_floor.tgz
-wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz
-wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk2.tgz
-wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_room.tgz
